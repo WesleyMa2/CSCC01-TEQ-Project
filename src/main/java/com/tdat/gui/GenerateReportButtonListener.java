1551e5825f0143b48db47c1bca2e21b470ac3c28
Radu Laudat <radu.laudat@gmail.com>
2018-10-30 04:14:48 -0400

    adding some code to the Generate Report Button Listener so that it is capable of doing more towards it's intended goal

--- src/main/java/com/tdat/gui/GenerateReportButtonListener.java
+++ src/main/java/com/tdat/gui/GenerateReportButtonListener.java
@@ -2,19 +2,29 @@ package com.tdat.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import com.tdat.data.JsonConverter;
+
 
 /*
  * When the "Generate Report" button is clicked, the code in this.actionPerformed() is executed 
  */
 public class GenerateReportButtonListener implements ActionListener{
+	JsonConverter jsonConverterInstance;
+	ArrayList<String> jsonObjectSerializedToString = new ArrayList<String>();
 	
 	public GenerateReportButtonListener(){
+		jsonConverterInstance = new JsonConverter();
 	}
 	
     public void actionPerformed(ActionEvent e) {
-    	//add Bekzod's JSON file generator in actionPerformed
-    	//If a file is selected in the JList, generate a report for that single uploaded file as mapped in fileUploadDict in GUI.java
-    	//If no file is selected in the JList, generate a report based on all uploaded files.
+    	/* add Bekzod's JSON file generator in actionPerformed
+    	 * If a file is selected in the JList, generate a report for that single uploaded file as mapped in fileUploadDict in GUI.java
+    	 * If no file is selected in the JList, generate a report based on all uploaded files. 
+    	 */
+    	jsonObjectSerializedToString.add(jsonConverterInstance.SerializeObject(GUI.columnValueInstanceCounterMap));
+    	
     }
     
 
