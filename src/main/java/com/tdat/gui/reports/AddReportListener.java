4de8aa8a957191883de3499bb8e3d53b148bc682
Alvin Tang <alvin.tang@mail.utoronto.ca>
2018-11-10 22:19:05 -0500

    Added all four listeners used in the ReportsPanel. Modified .gitignore such that it would not ignore the listeners in its reports folder/package.

---
+++ src/main/java/com/tdat/gui/reports/AddReportListener.java
@@ -0,0 +1,13 @@
+package com.tdat.gui.reports;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddReportListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+
+}
---
+++ src/main/java/com/tdat/gui/reports/RemoveReportListener.java
@@ -0,0 +1,14 @@
+package com.tdat.gui.reports;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RemoveReportListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
