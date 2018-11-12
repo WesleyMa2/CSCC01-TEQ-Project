6452801bdd5f7fcd80ba5cb392702b3a0ceff664
Bekzod <5884410+Bekzod13@users.noreply.github.com>
2018-11-12 13:44:13 -0500

    Revert "adding changes required to implement the early stages of the conflict…"

--- src/main/java/com/tdat/gui/UploadButtonListener.java
+++ src/main/java/com/tdat/gui/UploadButtonListener.java
@@ -4,21 +4,16 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.Year;
 import java.util.Calendar;
-import java.util.List;
-import java.util.Map;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
 
 import com.tdat.app.App;
-import com.tdat.data.*;
+import com.tdat.data.MasterData;
 import com.tdat.feeder.Uploader;
-import com.tdat.feeder.XLSXDataFileReader;
 
 /**
  * When upload is clicked, performs sends info back to main.
@@ -28,7 +23,6 @@ public class UploadButtonListener implements ActionListener {
     private String selectedYear;
     private String selectedFileType;
     private File selectedFile;
-    public static List<Map<String, String>> newUpload;
 
     public UploadButtonListener(){
         jfc.setCurrentDirectory(new File(System.getProperty("user.dir")));
@@ -37,46 +31,27 @@ public class UploadButtonListener implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         int returnValue = jfc.showOpenDialog(null);
+
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             this.selectedFile = jfc.getSelectedFile();
             App.selectedFile = selectedFile;
-            XLSXDataFileReader readNewUpload = new XLSXDataFileReader();
-            try {
-				newUpload = readNewUpload.converter(selectedFile);
-				System.out.println(newUpload);
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-            
-            ConflictIdentifier conflictCheck = new ConflictIdentifier();
-            //pass in the newUpload list here
-            conflictCheck.checkForAutomaticConflicts(newUpload);
-            int numManualConflicts = conflictCheck.numManualConflicts;
-            int numAutomaticConflicts = conflictCheck.numAutomaticallyResolvedConflicts;
-            
-            if ((numManualConflicts > 0) || (numAutomaticConflicts > 0)) {
-            	new ConflictWindow();
-            } else if ((numManualConflicts == 0) && (numAutomaticConflicts == 0)){
-                String timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
-                String historyEntry = "Date: " + timeStamp + "   Filename: " + selectedFile.getName() + "   Filetype: " + App.selectedFileType +
-                					"   Fiscal year: " + App.selectedYear;
-                UploadPanel.DLM.addElement(historyEntry);
-                App.fileUploadDict.put(historyEntry, this.selectedFile);
-                System.out.println("\n[File Uploaded]");
-                System.out.println(" File:\t\t" + App.selectedFile.getName());
-                System.out.println(" Type:\t" + App.selectedFileType);
-                System.out.println(" Path:\t" + selectedFile.getAbsolutePath());
-                System.out.println(" Year:\t" + App.selectedYear);
-                if (!Uploader.upload(App.selectedYear, App.selectedFile)){
-                    System.out.println("File Not Found!");
-                };
-                MasterData.printYearData(App.selectedYear);
-            	
-            }
+            String timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
+            String historyEntry = "Date: " + timeStamp + "   Filename: " + selectedFile.getName() + "   Filetype: " + App.selectedFileType +
+            					"   Fiscal year: " + App.selectedYear;
+            UploadPanel.DLM.addElement(historyEntry);
+            App.fileUploadDict.put(historyEntry, this.selectedFile);
+            System.out.println("\n[File Uploaded]");
+            System.out.println(" File:\t\t" + App.selectedFile.getName());
+            System.out.println(" Type:\t" + App.selectedFileType);
+            System.out.println(" Path:\t" + selectedFile.getAbsolutePath());
+            System.out.println(" Year:\t" + App.selectedYear);
+
+
+            if (!Uploader.upload(App.selectedYear, App.selectedFile)){
+                System.out.println("File Not Found!");
+            };
+            MasterData.printYearData(App.selectedYear);
+
         }
     }
 
