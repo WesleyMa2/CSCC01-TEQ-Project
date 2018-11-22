6eb83421f67ca222daf3161fb8aab0321ab01073
Wesley Ma <wesleykhma@gmail.com>
2018-11-22 02:42:42 -0500

    Implemented TrendChartScheme, and hooked it up to GUI

--- src/main/java/com/tdat/gui/reports/AddDistributionReportListener.java
+++ src/main/java/com/tdat/gui/reports/AddDistributionReportListener.java
@@ -3,15 +3,10 @@ package com.tdat.gui.reports;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import com.tdat.app.App;
-import com.tdat.report.chart.ChartScheme;
-import com.tdat.report.chart.ChartType;
-import com.tdat.report.chart.DistributionChartScheme;
-
 public class AddDistributionReportListener implements ActionListener {
-
 	public void actionPerformed(ActionEvent arg0) {
-		new AddDistributionReportWindow();
+		AddChartWindow.typeOfChartData = "Distribution";
+		new AddChartWindow();
 	}
 
 }
--- src/main/java/com/tdat/gui/reports/AddTrendsReportListener.java
+++ src/main/java/com/tdat/gui/reports/AddTrendsReportListener.java
@@ -4,9 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddTrendsReportListener implements ActionListener {
-
 	public void actionPerformed(ActionEvent arg0) {
-		new AddTrendsReportWindow();
+		AddChartWindow.typeOfChartData = "Trend";
+		new AddChartWindow();
 	}
 
 }
