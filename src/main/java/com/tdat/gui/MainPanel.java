77693bb291162e23f4eb42af98b54c339bcd6afc
Alvin Tang <alvin.tang@mail.utoronto.ca>
2018-11-25 03:04:20 -0500

    Modified the MainPanel such that it now has more user friendly instructions on how to get started.

--- src/main/java/com/tdat/gui/MainPanel.java
+++ src/main/java/com/tdat/gui/MainPanel.java
@@ -16,7 +16,15 @@ public class MainPanel extends GenericPanel {
 				+ "<h1>" + App.appTitle + "</h1>"
 				+ "<hr/>"
 				+ "<br/>"
-				+ "<p><b>Greetings!</b> Navigate through the upload and reports section using the tabs on the left.</p>\r\n" + 
+				+ "<p><b>Greetings!</b> Here is some basic information on how to get started! </p><br/>" +
+				"<p><b>Step 1.</b> Go to the 'Upload' tab to upload files, while specifying the file type and the file's related fiscal year for each file.</p><br/>" +
+				"<p><b>Step 2.</b> Go to the 'Reports' tab to generate a report with visual charts.</p>\r\n" +
+				"<p>Add charts, and modify the report using the TDATQL (TDAT Query Language).</p>" +
+				"<p>Add distribution, and trend charts of a specific column.</p>" +
+				"<p>The templates dropdown below the 'Reports to be generated' table gives you the option to add charts that are frequently used.</p>" +
+				"<p>Finally, generate a report in your browser using the 'Generate Report' button.</p><br/>" +
+				"<p><b>Optional:</b> Go to the 'Public data' tab to add charts with public data in the report for comparison.</p>" +
+				"<p>Make sure you do this before generating the report.</p>" +
 				"<br/>\r\n" + 
 				"<small>\r\n" + 
 				"<b>Acknowledgements:</b><br/>\r\n" + 
